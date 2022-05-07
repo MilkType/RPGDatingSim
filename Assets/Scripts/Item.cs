3205061ee4824314cda42493e9999e13639aaf1a
Plank8 <esassine@haverford.edu>
2022-05-07 02:39:06 -0400

    Add Silver walk Add new tileset Mid Inventory

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName ="Data/Item")]
+
+public class Item : ScriptableObject
+{
+    public string Name;
+    public string Description;
+    public bool Stackable;
+    public Sprite Icon;
+}
+
